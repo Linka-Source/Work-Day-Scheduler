4acc3e7c27e6505ab30832d0a57c35de2bc2cb56
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-04-05 20:21:19 +0930

    created function for current date display

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,10 @@
 let currentTime = dayjs().format('MMMM D, YYYY');
 console.log(currentTime);
+
+function displayTime(){
+    document.getElementById("currentDay").innerHTML = currentTime;
+    
+};
+
+setInterval(displayTime, 1000);
+displayTime()
