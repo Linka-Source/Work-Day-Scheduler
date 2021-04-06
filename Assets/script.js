9ed89f09f3bad1297856f14ea49aeae11c39dc54
Paulina Koczwara <paulinka.koczwara@gmail.com>
2021-04-06 18:34:50 +0930

    tried editing function

--- Assets/script.js
+++ Assets/script.js
@@ -6,6 +6,30 @@ function displayTime(){
     
 };
 
+// function timeTracker() {
+//     var timeNow = dayjs().hour();
+
+//     $(".time-block").each(function () {
+//         var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+//         if (blockTime < timeNow) {
+//             $(this).removeClass("future");
+//             $(this).removeClass("present");
+//             $(this).addClass("past");
+//         }
+//         else if (blockTime === timeNow) {
+//             $(this).removeClass("past");
+//             $(this).removeClass("future");
+//             $(this).addClass("present");
+//         }
+//         else {
+//             $(this).removeClass("present");
+//             $(this).removeClass("past");
+//             $(this).addClass("future");
+
+//         }
+//     })
+
 setInterval(displayTime, 1000);
 displayTime()
 
